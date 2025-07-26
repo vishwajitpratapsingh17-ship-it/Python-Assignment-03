aca45d2e36406df200bc7a2f05a070cf347e16c5
vishwajitpratapsingh17-ship-it <vishwajitpratapsingh17@gmail.com>
2025-07-26 10:11:31 +0530

    Update python-assignment-03.py

--- python-assignment-03.py
+++ python-assignment-03.py
@@ -7,21 +7,3 @@ def factorial(n):
 result = factorial(n)
 print(result)
 
-print()
-
-import math
-n=int(input("Please enter a random number? :  "))
-def sqrt(n):
-    return math.sqrt(n)
-result = sqrt(n)
-print(result)
-print()
-def log(n):
-    return math.log(n)
-result = log(n)
-print(result)
-print()
-def sin(n):
-    return math.sin(n)
-result = sin(n)
-print(result)
